92c23cbcd2146ea53e46c4bfa8b898afffe9dd04
Joshua Richards <josh.jrdba@gmail.com>
2025-07-16 15:55:48 -0700

    Working build with Tailwind

--- src/components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Card({ children }) {
-  return (
-    <div className="rounded-2xl p-4 shadow-md bg-white border border-gray-200">
-      {children}
-    </div>
-  );
-}
